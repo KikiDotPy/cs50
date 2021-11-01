7795c81effeb3b7ebc4139000a74a6595dce6a33
Chiara <81535309+Saychan@users.noreply.github.com>
2021-11-01 17:51:28 +0100

    Create additino.py

---
+++ Week6/lecture_exercise/additino.py
@@ -0,0 +1,12 @@
+# Addition with int [using get_int]
+ 
+fromt cs50 import get_int
+ 
+# Prompt user for x
+x = get_int("x: ")
+
+# Promtp user for y
+y = get_int("y: ")
+
+# Perform addition
+print(x + y)
