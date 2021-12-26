7ee65170d9e3527cc8085ad277cae08a08252138
Chiara <81535309+KikiDotPy@users.noreply.github.com>
2021-12-26 16:56:53 +0100

    Add files via upload

---
+++ Week7/ProblemSet/Movies/12.sql
@@ -0,0 +1,7 @@
+SELECT title
+FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id 
+WHERE name IN ('Johnny Depp', 'Helena Bonham Carter') 
+GROUP BY movies.id, movies.title
+HAVING COUNT(*) = 2;
