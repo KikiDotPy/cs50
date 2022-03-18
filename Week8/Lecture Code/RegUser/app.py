db9f10fc8e3f3e9b51610227a2651834e37334f1
KikiDotPy <1chiara.disano@gmail.com>
2022-03-18 20:57:32 +0100

    setup form in index.html

--- Week8/Lecture Code/RegUser/app.py
+++ Week8/Lecture Code/RegUser/app.py
@@ -1,1 +1,11 @@
 from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+SPORT = ["Tennis", "Football", "Rugby", "Golf"]
+REGISTRANT = {}
+
+@app.route("/", methods=["POST"])
+def index():
+    return render_template("index.html", sports=SPORT)
+
