8566b8f1f57e02cfeff96070f8df64fc7ea82d66
KikiDotPy <1chiara@libero.it>
2022-03-18 17:38:37 +0100

    fix on Flask greet

--- Week8/Lecture Code/Greet/app.py
+++ Week8/Lecture Code/Greet/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("greet.html")
+@app.route("/greet")
 def greet():
     name = request.args.get("name")
     return render_template("greet.html", name=name)
