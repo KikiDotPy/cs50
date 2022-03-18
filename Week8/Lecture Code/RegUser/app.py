dccba7541f7eb6bf1c0de629bc49a894d30e2559
KikiDotPy <1chiara.disano@gmail.com>
2022-03-18 21:09:01 +0100

    app.py final

--- Week8/Lecture Code/RegUser/app.py
+++ Week8/Lecture Code/RegUser/app.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 SPORT = ["Tennis", "Football", "Rugby", "Golf"]
 REGISTRANT = {}
 
-@app.route("/", methods=["POST"])
+@app.route("/")
 def index():
     return render_template("index.html", sports=SPORT)
 
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+
+    if not name:
+        return render_template("error.html", message="Missing name")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORT:
+        return render_template("error.html", message="Invalid sport")
+    
+    REGISTRANT[name] = sport
+
+    return render_template("registrant.html", registrant=REGISTRANT)
