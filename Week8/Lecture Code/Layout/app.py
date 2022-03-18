168a1d2e6e28d1cd1c118f84488733d55d18d3b9
KikiDotPy <1chiara.disano@gmail.com>
2022-03-18 17:56:09 +0100

    setup Flask application with layout

--- Week8/Lecture Code/Layout/app.py
+++ Week8/Lecture Code/Layout/app.py
@@ -1,1 +1,12 @@
-cod
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name")
+    return render_template("greet.html", name=name)
