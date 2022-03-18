e914c46944f57c680939aba3c656c0912706e170
KikiDotPy <1chiara.disano@gmail.com>
2022-03-18 18:51:17 +0100

    added flask logic

--- Week8/Lecture Code/Registration/app.py
+++ Week8/Lecture Code/Registration/app.py
@@ -10,7 +10,18 @@ SPORT = [
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORT)
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+
+    if not name or sport not in SPORT:
+        return render_template("failure.html")
+    else:
+        return render_template("success.html")
+
 
 
 
